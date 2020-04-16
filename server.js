73d0f609722a24f656ba0b1135382d2169b1598a
Andrei Cocota <andrei_real2000@yahoo.com>
2020-04-16 16:29:20 +0300

    updates to deploy heroku

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-
-const path = require("path");
-const express = require("express");
+const express = require('express');
+const path = require('path');
+ 
 const app = express();
-app.use(express.static(__dirname + '/ng2-cribs'));
-app.get('/*', function(req,res){
-res.sendFile(path.join(__dirname, 'ng2-cribs', 'index.html'))
+ 
+// Serve only the static files form the angularapp directory
+app.use(express.static(__dirname + '/dist/ng2-cribs'));
+ 
+app.get('/*', function(req,res) {
+ 
+res.sendFile(path.join(__dirname+'/dist/ng2-cribs/index.html'));
 });
-
+ 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
